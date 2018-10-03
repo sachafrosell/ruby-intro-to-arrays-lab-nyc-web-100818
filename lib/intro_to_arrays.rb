1b7d1c7c0cd33ed0c0b69a763e121b5f1ec50476
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 21:50:29 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,21 @@
 def instantiate_new_array
   []
 end
+
+def array_with_two_elements
+  ["dog", "cat"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+  
+def last_element(array)
+  array[array.length - 1]
+end
+
+def
