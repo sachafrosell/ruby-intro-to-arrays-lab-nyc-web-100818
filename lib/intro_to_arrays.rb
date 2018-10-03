812702051c386d1b9f88378554de26b4eac94364
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 21:53:25 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,8 +19,7 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.shift()
-  array.shift()
+  first_element = array.shift()
 end
 
 def last_element_with_array_methods(array)
